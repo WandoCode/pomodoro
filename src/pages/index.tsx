3fd6bc29c193503191c1bbe1334e8db0ae500e78
Maxime Chirez <mchirez@gmail.com>
2023-05-01 20:59:49 +0200

    Add main variables for fonts and colors

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,9 @@
-import Head from 'next/head'
-import Image from 'next/image'
-
 export default function Home() {
   return (
     <>
-      <main>APP</main>
+      <main>
+        <h1 className="h1">APP</h1>
+      </main>
     </>
   )
 }
